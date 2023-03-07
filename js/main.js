0817aeaf84d192dd22c92024f67935ead6cbc336
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-07 15:33:17 +0100

    creation of the object array

---
+++ js/main.js
@@ -0,0 +1,46 @@
+'use strict';
+
+////////////
+// Functions
+////////////
+
+// A function to create a team member object containing their name, role and photo
+function createTeamMember(fullName, memberRole, memberPhoto) {
+  const teamMember = {
+    name: fullName,
+    role: memberRole,
+    photo: memberPhoto,
+  };
+
+  return teamMember;
+}
+
+////////////
+// Main
+////////////
+
+// For each team member, we can create an object will all the information needed
+const wayneBarnett = createTeamMember('Wayne Barnett', 'Founder & CEO', 'wayne-barnett-founder-ceo.jpg');
+const angelaCaroll = createTeamMember('Angela Caroll', 'Chief Editor', 'angela-caroll-chief-editor.jpg');
+const walterGordon = createTeamMember('Walter Gordon', 'Office Manager', 'walter-gordon-office-manager.jpg');
+const angelaLopez = createTeamMember('Angela Lopez', 'Social Media Manager', 'angela-lopez-social-media-manager.jpg');
+const scottEstrada = createTeamMember('Scott Estrada', 'Developer', 'scott-estrada-developer.jpg');
+const barbaraRamos = createTeamMember('Barbara Ramos', 'Graphic Designer', 'barbara-ramos-graphic-designer.jpg');
+
+// Creation of an array to contain the different staff members
+const teamMembers = [wayneBarnett, angelaCaroll, walterGordon, angelaLopez, scottEstrada, barbaraRamos];
+
+console.log(teamMembers);
+
+
+
+// To check if the code is correct, we can then console print all the single information for every team member
+
+
+// To transform our project into a visual one, we can then print all the information as strings in our DOM
+
+
+// To give our project its final look, we can transform the photo string into an image
+
+
+// Finally, we can use CSS to create the style of our final page
